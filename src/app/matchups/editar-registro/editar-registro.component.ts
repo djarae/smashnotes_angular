64d48c8c45f9de05e8e51009420c6b9c060384df
djarae <51816647+djarae@users.noreply.github.com>
2025-03-29 20:15:32 -0300

    Obtencion data update :)

--- src/app/matchups/editar-registro/editar-registro.component.ts
+++ src/app/matchups/editar-registro/editar-registro.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { url_entorno } from '../../../configs/url_entorno';
 import { personajeService } from '../../../services/matchups/personaje.service';
 import { escenarioService } from '../../../services/matchups/escenario.service';
@@ -13,19 +13,25 @@ export class EditarRegistroComponent {
   selectPersonaje: string[] = [];
   lstPersonajes: any;
   lstEscenarios: any;
-
+  //Bindings de los select
   selectedPersonaje: any =0;  // Variable para almacenar el valor seleccionado
   selectedEscenario: any = '';  // Variable para almacenar el valor seleccionado
   porcentajeKo: any = '';  // Variable para almacenar el valor seleccionado
+  //Props desde el listado
+  @Input() idPjReceptor!: number;
+
 
   async ngOnInit() {
     this.lstPersonajes = await new personajeService().getPersonajes();
     this.lstEscenarios = await new escenarioService().getEscenarios();
+    this.selectedPersonaje = this.idPjReceptor;
   }
 
   async  updatePorcentajeKO() {
     console.log("Ingresamos a insertar ");
-    const response = await new registroService().insertarRegistro(this.selectedPersonaje,this.selectedEscenario,this.porcentajeKo);
-   }
+    console.log("Receptor ,escenario y porcentaje son: ",this.selectedPersonaje,this.selectedEscenario,this.porcentajeKo);
+    //  const response = await new registroService().insertarRegistro(this.selectedPersonaje,this.selectedEscenario,this.porcentajeKo);
+   
+  }
 
 }
