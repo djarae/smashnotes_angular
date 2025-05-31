14d548accf0c787446f650668a08667dcf2eef94
DiegoJaraEstivill <diego.jara@codde.cl>
2025-05-31 05:34:07 -0400

    url

--- src/configs/url_entorno.ts
+++ src/configs/url_entorno.ts
@@ -1,7 +1,7 @@
 export function url_entorno() {
    //production:
   // const url_entorno = "https://smashnotes-springboot-back-1.onrender.com";
-  const url_entorno = "https://smashnotes-back-1.onrender.com";
+  const url_entorno = "https://smashnotes-back.onrender.com";
    //development
  //const url_entorno = 'http://127.0.0.1:8080';
 
