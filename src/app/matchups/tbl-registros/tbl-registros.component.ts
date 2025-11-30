fcdd76f12c1af2e3699dff48453626850c02fb29
djarae <djaraestivill@gmail.com>
2025-11-30 05:00:09 -0300

    feat

--- src/app/matchups/tbl-registros/tbl-registros.component.ts
+++ src/app/matchups/tbl-registros/tbl-registros.component.ts
@@ -1,5 +1,5 @@
-import { Component , OnInit } from '@angular/core';
-import {url_entorno} from '../../../configs/url_entorno';
+import { Component, OnInit } from '@angular/core';
+import { url_entorno } from '../../../configs/url_entorno';
 import { registroService } from 'src/services/matchups/registro.service';
 
 @Component({
@@ -7,32 +7,40 @@ import { registroService } from 'src/services/matchups/registro.service';
   templateUrl: './tbl-registros.component.html',
   styleUrls: ['./tbl-registros.component.css']
 })
-export class TblRegistrosComponent  implements OnInit {
-  LstRegistros : any;
+export class TblRegistrosComponent implements OnInit {
+  LstRegistros: any;
 
   //filtros:
   filtroEmisor: any;
-  filtroReceptor: any;  
-  filtroMov:any;
-  filtroRage:any;
-  filtroPosicion:any;
-  filtroStage:any;
-  filtroConDI:any;
-  filtroSinDI:any;
+  filtroReceptor: any;
+  filtroMov: any;
+  filtroRage: any;
+  filtroPosicion: any;
+  filtroStage: any;
+  filtroConDI: any;
+  filtroSinDI: any;
 
   async ngOnInit() {
-    await this.cargarRegistros()
+    // Limpiar filtros al inicializar
+    this.filtroEmisor = null;
+    this.filtroReceptor = null;
+    this.filtroMov = null;
+    this.filtroRage = null;
+    this.filtroPosicion = null;
+    this.filtroStage = null;
+
+    await this.cargarRegistros();
   }
 
   async cargarRegistros() {
     console.log("Inicio cargar registros")
-    console.log("this filtro emisor es ",this.filtroEmisor)
-    console.log("this filtro receptor es ",this.filtroReceptor)
-    console.log("this filtro mov es ",this.filtroMov)
-    console.log("this filtro rage es ",this.filtroRage)
-    console.log("this filtro posicion es ",this.filtroPosicion)
-    console.log("this filtro stage es ",this.filtroStage)
-    const response =  await new registroService().getAllRegistros(
+    console.log("this filtro emisor es ", this.filtroEmisor)
+    console.log("this filtro receptor es ", this.filtroReceptor)
+    console.log("this filtro mov es ", this.filtroMov)
+    console.log("this filtro rage es ", this.filtroRage)
+    console.log("this filtro posicion es ", this.filtroPosicion)
+    console.log("this filtro stage es ", this.filtroStage)
+    const response = await new registroService().getAllRegistros(
       this.filtroEmisor,
       this.filtroReceptor,
       this.filtroRage,
@@ -41,6 +49,6 @@ export class TblRegistrosComponent  implements OnInit {
       this.filtroMov
     );
     this.LstRegistros = await response; this.LstRegistros = [...this.LstRegistros];
-     console.log(this.LstRegistros,"LstRegistros");
+    console.log(this.LstRegistros, "LstRegistros");
   }
 }
